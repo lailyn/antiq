5f50aa49be9e84e33c1410ca9b959454d675b0c0
Lailyn Puad <lailynfuad@gmail.com>
2020-12-23 09:30:42 +0700

    dashboard

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -1,3 +1,17 @@
+<?php  
+function mata_uang($a){      
+  if(is_numeric($a) AND $a != 0 AND $a != ""){
+    return number_format($a, 0, ',', '.');
+  }else{
+    return $a;
+  }
+}
+function manipulasiTanggal($tgl,$jumlah=-1,$format='days'){
+  $currentDate = new DateTime($tgl);
+  $currentDate -> modify($jumlah.' '.$format);
+  return $currentDate -> format('Y-m-d');
+}
+?>
 <div class="main-panel">
   <div class="content-wrapper">    
 
@@ -16,9 +30,14 @@
         <div class="card bg-gradient-info card-img-holder text-white">
           <div class="card-body">
             <img src="assets/images/dashboard/circle.svg" class="card-img-absolute" alt="circle-image" />
-            <h4 class="font-weight-normal mb-3">Dokter <i class="mdi mdi-shopping mdi-24px float-right"></i>
+            <h4 class="font-weight-normal mb-3">Saldo Kemarin <i class="mdi mdi-shopping mdi-24px float-right"></i>
             </h4>
-            <h2 class="mb-5"><?php echo $this->m_admin->getAll("md_user")->num_rows(); ?> orang</h2>              
+            <?php 
+            $hari = date("Y-m-d"); 
+            $kemarin = manipulasiTanggal($hari);             
+            ?>
+            <h2 class="mb-5">Debit: <?php echo mata_uang($this->db->query("SELECT SUM(debit) AS jum FROM md_transaksi WHERE tanggal = '$kemarin'")->row()->jum); ?></h2>              
+            <h2 class="mb-5">Kredit: <?php echo mata_uang($this->db->query("SELECT SUM(kredit) AS jum FROM md_transaksi WHERE tanggal = '$kemarin'")->row()->jum); ?></h2>              
           </div>
         </div>
       </div>
@@ -26,9 +45,10 @@
         <div class="card bg-gradient-success card-img-holder text-white">
           <div class="card-body">
             <img src="assets/images/dashboard/circle.svg" class="card-img-absolute" alt="circle-image" />
-            <h4 class="font-weight-normal mb-3">Layanan <i class="mdi mdi-animation mdi-24px float-right"></i>
+            <h4 class="font-weight-normal mb-3">Saldo Hari Ini <i class="mdi mdi-animation mdi-24px float-right"></i>
             </h4>
-            <h2 class="mb-5"><?php echo $this->m_admin->getAll("md_user")->num_rows(); ?> jenis</h2>            
+            <h2 class="mb-5">Debit: <?php echo mata_uang($this->db->query("SELECT SUM(debit) AS jum FROM md_transaksi WHERE tanggal = '$hari'")->row()->jum); ?></h2>              
+            <h2 class="mb-5">Kredit: <?php echo mata_uang($this->db->query("SELECT SUM(kredit) AS jum FROM md_transaksi WHERE tanggal = '$hari'")->row()->jum); ?></h2>              
           </div>
         </div>
       </div>
